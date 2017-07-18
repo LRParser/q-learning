a82fde0c2c34cd247ad2e07d4a3fb3500bd2de88
Joseph Heenan <jheenan@greenkeytech.com>
2017-07-18 11:43:30 +0200

    Improved safety

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -19,6 +19,8 @@ class LearningAgent(Agent):
         self.epsilon = epsilon   # Random exploration factor
         self.alpha = alpha       # Learning factor
 
+        self.t = 0
+
         ###########
         ## TO DO ##
         ###########
@@ -44,7 +46,13 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else :
-            self.epsilon = self.epsilon - 0.05
+            self.t += 1
+            # Linear decay:
+            # self.epsilon = self.epsilon - 0.05
+
+            self.epsilon *= .9
+            # Decay to tolerance
+            # self.epsilon = math.pow(self.alpha,self.t) #1 / (self.t * self.t) # math.exp(-1 * self.alpha * self.t)
 
         return None
 
@@ -212,7 +220,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent)
+    agent = env.create_agent(LearningAgent,epsilon=1.0,alpha=0.8)
     
     ##############
     # Follow the driving agent
@@ -227,14 +235,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env,update_delay=0.01, display=False, log_metrics=True)
+    sim = Simulator(env,update_delay=0.01, display=False, log_metrics=True, optimized=True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(tolerance=0.05,n_test=10)
 
 
 if __name__ == '__main__':
